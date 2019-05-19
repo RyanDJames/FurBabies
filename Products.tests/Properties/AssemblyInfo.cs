abc35e377d3eccced1245305835f265c9f9431e5
Ryan James <james.ryan.d@outlook.com>
2019-05-19 17:41:29 -0400

    BasicTest

---
+++ Products.tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Products.tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Products.tests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f78fd6cf-94b0-4dd6-a654-9044d6702c9f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Products.tests/UnitTest1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Products.tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+
+        private IReservationHelper getTestObject()
+        {
+            return new ReservationHelper();
+        }
+        [TestMethod]
+        public void Reservation_beyond_180()
+        {
+            IReservationHelper target = getTestObject();
+            int total = 180;
+
+
+            var daysAwayTotal = target.ScheduleReservation(daysAwayTotal);
+
+            Assert.AreEqual(daysAwayTotal > 180, days)
+           
+        }
+    }
+}
